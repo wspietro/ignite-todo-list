5515d1d7045c954aebda8dace379a7a4e4c0b2e3
Pietro Weg <pietro.sera@compasso.com.br>
2022-07-03 21:17:55 -0300

    feat: initial scaffold

---
+++ src/Constants.tsx
@@ -0,0 +1,23 @@
+export const COLORS = {
+  produto: {
+    purple: '#8284fa',
+    purpleDark: '#5E60CE',
+    blue: '#8284fa',
+    blueDark: '#8284fa',
+  },
+  gray: {
+    100: '#F2F2F2',
+    200: '#D9D9D9',
+    300: '#808080',
+    400: '#333333',
+    500: '#262626',
+    600: '#1A1A1A',
+    700: '#0D0D0D',
+  },
+  danger: '#E25858'
+}
+
+export const WEIGHTS = {
+  normal: 400,
+  bold: 700
+}
