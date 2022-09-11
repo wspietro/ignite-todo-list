dabdf6c996154d99fd467e6daa0f77d6a0b9de22
Pietro Weg <pietro.sera@compasso.com.br>
2022-09-11 20:03:35 -0300

    feat: task-list-header v1 desenvolvido
    header com controle de tarefas criadas e concluidas finalizado em layout. Futuramente terá lógica para o contador

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,6 +1,50 @@
+import styled from "styled-components";
+import { COLORS, FONT_SIZE, WEIGHTS } from "../styles/Constants"
 
 export function TaskList() {
   return (
-    <div>TaskList</div>
+    <TaskListWrapper>
+      <TaskListHeader>
+        <p>Tarefas Criadas <Counter>0</Counter></p>
+        <p>Concluídas <Counter>0</Counter></p>
+      </TaskListHeader>
+    </TaskListWrapper>
   );
 };
+
+
+export const TaskListWrapper = styled.section`
+  width: 46.125rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+`
+
+export const TaskListHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+    p {
+      font-weight: ${WEIGHTS.bold};
+      font-size: ${FONT_SIZE.md};
+      color: ${COLORS.produto.blue};
+      line-height: 1.0625rem;
+
+        :last-child {
+          color: ${COLORS.produto.purple};
+        }
+    }
+`
+
+export const Counter = styled.span`
+  color: ${COLORS.gray[200]};
+  font-size: ${FONT_SIZE.sm};
+  line-height: 0.9375rem;
+
+  background-color: ${COLORS.gray[400]};
+  padding: 2px 8px;
+  border-radius: 999px;
+
+  margin-left: 0.5rem;
+` 
