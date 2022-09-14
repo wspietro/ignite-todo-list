b7c5e346c29408a6770d1a31cb93eebf41d39ba4
Pietro Weg <pietro.sera@compasso.com.br>
2022-09-14 18:51:56 -0300

    feat: checkbox component estrutura inicial
    necessário ajustes para que o mesmo fique redondo e siga os estilos do figma

--- src/components/CheckboxIcon.tsx
+++ src/components/CheckboxIcon.tsx
@@ -1,14 +1,25 @@
 import { Checkbox } from '@mui/material';
-import { COLORS } from '../styles/Constants'
+import { COLORS } from '../styles/Constants';
+
+const checkboxStyles = {
+  color: COLORS.produto.blue,
+  '&:hover': {
+    color: COLORS.produto.blueDark,
+    backgroundColor: 'initial',
+  },
+  '&.Mui-checked': {
+    color: COLORS.produto.purpleDark,
+    '&:hover': {
+      color: COLORS.produto.purple
+    }
+  },
+}
 
 function CheckboxIcon() {
   return (
-    <Checkbox sx={{
-      color: COLORS.produto.blue,
-      '&:hover': {
-        color: COLORS.produto.blueDark,
-      },
-    }} />
+    <>
+      <Checkbox sx={checkboxStyles} />
+    </>
   )
 }
 
