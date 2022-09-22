8b9dd5ecefb9605558e3f5d8609495d35f268f70
Pietro Weg <pietro.sera@compasso.com.br>
2022-09-21 23:41:02 -0300

    feat: estrutura inicial contexto tasks

---
+++ src/contexts/TasksContext.tsx
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react"
+
+
+interface TasksContextType {
+
+}
+
+export const TasksContext = createContext({} as TasksContextType)
+
+
+export function TasksContextProvider() {
+
+}
