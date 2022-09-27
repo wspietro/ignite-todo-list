c78fb1335cfbdeed57def98940e117b907b5dfc3
Pietro Weg <pietro.sera@compasso.com.br>
2022-09-26 22:32:47 -0300

    feat: criando contexto e funcao create-new-task

--- src/contexts/TasksContext.tsx
+++ src/contexts/TasksContext.tsx
@@ -1,13 +1,47 @@
 import { createContext, useState } from "react"
+import { v4 as uuidv4 } from "uuid";
 
+interface TaskState {
+  id: string;
+  createdAt: Date;
+  taskContent: string;
+  completedAt?: Date;
+}
 
 interface TasksContextType {
+  createNewTask: (inputValue: string) => void;
+}
 
+interface CyclesContextProviderProps {
+  children: React.ReactNode;
 }
 
 export const TasksContext = createContext({} as TasksContextType)
 
 
-export function TasksContextProvider() {
+export function TasksContextProvider({ children }: CyclesContextProviderProps) {
+  const [tasksList, setTasksList] = useState<TaskState[]>([]);
 
+  function createNewTask(inputValue: string) {
+    const id = uuidv4();
+    const taskContent = inputValue
+
+    const newTask: TaskState = {
+      id,
+      createdAt: new Date(),
+      taskContent,
+    };
+
+    setTasksList(prevState => [...prevState, newTask]);
+  }
+
+  return (
+    <TasksContext.Provider
+      value={{
+        createNewTask,
+      }}
+    >
+      {children}
+    </TasksContext.Provider>
+  )
 }
